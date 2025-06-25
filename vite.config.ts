3e6d4465ee6637075be98d8b4a8e8b9d0386b455
Mohammed Wael <mohammedsama2017@gmail.com>
2025-06-25 04:36:14 +0300

    editing vite file

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { visualizer } from "rollup-plugin-visualizer";
-import compression from 'vite-plugin-compression';
-
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), visualizer({ open: true }), compression()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("react")) return "vendor-react";
-            if (id.includes("chakra-ui")) return "vendor-chakra";
-            return "vendor";
-          }
-        },
-      },
-    },
-  },
+  plugins: [react(), tailwindcss()],
 });
