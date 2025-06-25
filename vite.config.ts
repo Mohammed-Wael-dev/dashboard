2bfd2d6da194e5364af527c03b185dbbaeecad38
Mohammed Wael <mohammedsama2017@gmail.com>
2025-06-25 04:00:24 +0300

    Fix folder casing for Vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,21 @@ import tailwindcss from "@tailwindcss/vite";
 import { visualizer } from "rollup-plugin-visualizer";
 import compression from 'vite-plugin-compression';
 
+
 export default defineConfig({
   base: './',
   plugins: [react(), tailwindcss(), visualizer({ open: true }), compression()],
   build: {
-    // rollupOptions: {
-    //   output: {
-    //     manualChunks(id) {
-    //       if (id.includes("node_modules")) {
-    //         if (id.includes("react")) return "vendor-react";
-    //         if (id.includes("chakra-ui")) return "vendor-chakra";
-    //         return "vendor";
-    //       }
-    //     },
-    //   },
-    // },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react")) return "vendor-react";
+            if (id.includes("chakra-ui")) return "vendor-chakra";
+            return "vendor";
+          }
+        },
+      },
+    },
   },
 });
